01eab968939e3376b8571aa003da1218a2257941
make_wish <mr.kocta@bk.ru>
2023-11-28 12:06:26 +0300

    (create): db conection

---
+++ src/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from src.config import DB_HOST, DB_NAME, DB_PORT, DB_PASSWORD, DB_USER
+
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
