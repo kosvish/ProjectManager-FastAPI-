a195d6294fbf4ca8b18ee60670bea19fc434ff15
make_wish <mr.kocta@bk.ru>
2023-12-11 14:22:12 +0300

    (add): new vars for test database

--- src/config.py
+++ src/config.py
@@ -8,3 +8,10 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
+
+
+DB_TEST_USER = os.getenv("DB_TEST_USER")
+DB_TEST_PASSWORD = os.getenv("DB_TEST_PASSWORD")
+DB_TEST_HOST = os.getenv("DB_TEST_HOST")
+DB_TEST_PORT = os.getenv("DB_TEST_PORT")
+DB_TEST_NAME = os.getenv("DB_TEST_NAME")
