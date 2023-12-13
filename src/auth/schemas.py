6164c546de8ee66cbefccd77598ece0a79592741
make_wish <mr.kocta@bk.ru>
2023-12-13 14:06:43 +0300

    (create): schemas for token

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -48,4 +48,18 @@ class UserResponse(BaseModel):
     hashed_password: str
 
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+
 
