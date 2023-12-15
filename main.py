9a375d259048eb07d17d686350509cb1fe341b91
make_wish <mr.kocta@bk.ru>
2023-12-15 16:21:55 +0300

    (add): after run app start redis

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from src.auth.router import router as user_router
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
+
 
 app = FastAPI(title="GitHub")
 
 app.include_router(user_router)
+
+
+@app.on_event("startup")
+async def startup_event():
+    redis = aioredis.from_url("redis://localhost", encoding="utf-8", decode_reponse=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
