3fb9353418054788be03bc04ad3643aead59818f
make_wish <mr.kocta@bk.ru>
2023-12-18 13:58:03 +0300

    (add): include verif rout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from src.auth.router import router as user_router
+from src.verefication.router import router as verif_router
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
@@ -8,10 +9,14 @@ from redis import asyncio as aioredis
 app = FastAPI(title="GitHub")
 
 app.include_router(user_router)
+app.include_router(verif_router)
 
 
 @app.on_event("startup")
 async def startup_event():
     redis = aioredis.from_url("redis://localhost", encoding="utf-8", decode_reponse=True)
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
+
+
 
